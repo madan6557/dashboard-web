cb1619bc13703530741d8c863b593d5d58e86ce2
madan6557 <muhammaarizky.mrar@gmail.com>
2025-02-10 11:28:20 +0800

    fix: email destination

--- src/container/UserDetails/UserDetails.jsx
+++ src/container/UserDetails/UserDetails.jsx
@@ -14,7 +14,6 @@ const UserDetails = ({ onClose, onAction, data }) => {
     const requestConfirmation = useConfirmation();
 
      const requestResetPassword = async () => {
-        const email = "2111016210015@mhs.ulm.ac.id";
             const data = {
                 email: email
             };
